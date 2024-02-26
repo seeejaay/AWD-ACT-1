0a34d74376f0543677c9c34883655b0a14f8b84d
Carl Justine Malabanan <carljustinem984@gmail.com>
2024-02-26 21:05:41 +0800

    Fixed JS

--- assets/JS/function.js
+++ assets/JS/function.js
@@ -1,6 +1,25 @@
 const buttonBG = document.getElementById("_buttonBG");
-const button
+const buttonJW = document.getElementById("_buttonJW");
+const buttonBN = document.getElementById("_buttonBN");
+const buttonFE = document.getElementById("_buttonFE");
+const buttonSB = document.getElementById("_buttonSB");
 
 buttonBG.addEventListener("click", () => {
   window.open("BackgroundAnimation/bgAnimation.html", "_self");
+});
+
+buttonJW.addEventListener("click", () => {
+  window.open("JumpWords/jumpwords.html", "_self");
+});
+
+buttonBN.addEventListener("click", () => {
+  window.open("Bell/Bell.html", "_self");
+});
+
+buttonFE.addEventListener("click", () => {
+  window.open("Illumination/illlumination.html", "_self");
+});
+
+buttonSB.addEventListener("click", () => {
+  window.open("Search/search.html", "_self");
 });
